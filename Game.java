5d17a26dccb68645d298c5f63433ca14059f3bc0
Gpoelsma <32401594+Gpoelsma@users.noreply.github.com>
2017-10-09 15:42:13 -0600

    Here's a JFrame
    There's a couple of extra bells and whistles from that Youtube tutorial that we'll probably need but I'm not sure how and where it will fit.

---
+++ Game.java
@@ -0,0 +1,111 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+
+
+
+public class Game implements Runnable
+{
+
+	private DisplayScreen display;
+	public int width, height;
+	public String title;
+	private boolean running = false;
+	private Thread thread;
+	
+	public BufferStrategy bs;
+	public Graphics g;
+	
+	public Game(String title, int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		this.title = title;
+		
+	}
+	
+	private void init()
+	{
+		display = new DisplayScreen(title, width, height);
+	}
+	
+	private void tick()
+	{
+		
+	}
+	
+	private void render()
+	{
+		bs = display.getCanvas().getBufferStrategy();
+		if(bs == null)
+		{
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
+		g = bs.getDrawGraphics();
+	// Here you create shapes and color them etc
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(0,0, width, height);
+	// Creates non-interactable platforms "platform"
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 550, width, 300);
+		
+		g.setColor(Color.GREEN);
+		g.fillRect(100, 450, 100, 15);
+		
+		g.setColor(Color.GREEN);
+		g.fillRect(300, 200, 100, 15);
+		
+		g.setColor(Color.GREEN);
+		g.fillRect(430, 250, 100, 15);
+		
+		
+		
+		//end draw and make them show
+		bs.show();
+		g.dispose();
+	}
+	
+	public void run()
+	{
+		init();
+		while(running)
+		{
+			tick();
+			render();	
+		}
+		
+		stop();
+	}
+	
+	public synchronized void start ()
+	
+	{	if (running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop ()
+	{	if(!running)
+		return;
+		 running =false;
+		 
+		 
+		try 
+	{
+		thread.join();
+		} catch (InterruptedException e) {
+		
+		e.printStackTrace();
+		}
+		
+	}
+	
+}
+
+
+
---
+++ Launcher.java
@@ -0,0 +1,13 @@
+
+
+public class Launcher 
+{
+	public static void main (String[] args)
+	
+	{
+	Game game = new Game("BearGame!", 900, 600);
+	game.start();
+	
+	}
+
+}
